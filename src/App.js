e2c67444a76f52e76936a8d3941645fbbbd75a17
Halle King <hnking515@gmail.com>
2024-12-03 12:48:37 -0700

    add about component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 import React from 'react';
 import Header from './components/header/Header';
+import About from './components/about/About';
+
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <About/>
     </div>
   );
 }
