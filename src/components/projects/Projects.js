ce7cc8e1ec0ba978e7c86b628237c4d927c6f96f
Halle King <hnking515@gmail.com>
2024-12-04 09:47:46 -0700

    Add project accordian

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -1,17 +1,11 @@
 import React from 'react';
-import ExpandableComponent from './ExpandableComponent';
+import ProjectAccordion from './ProjectAccordion';
 
 function Projects() {
   return (
     <div>
         <h3>Projects</h3>
-        <ExpandableComponent header="Ruled Out" content="An inductive reasoning game."/>
-        <ExpandableComponent header="Penguin Rally" content="A spatial orientation game."/>
-        <ExpandableComponent header="What's My Name" content="A memory game."/>
-        <ExpandableComponent header="FSX Pro" content="A golf fitting and coaching application."/>
-        <ExpandableComponent header="Wiz Royale" content="A multiplayer battle royale game."/>
-        <ExpandableComponent header="Macarena" content="A 3D animation"/>
-        <ExpandableComponent header="3D Animation" content="3D photorealistic animation."/>
+        <ProjectAccordion/>
     </div>
   );
 }
