57525154cc0240e9e2d4f29675abe61a1f01e3b8
Halle King <hnking515@gmail.com>
2025-02-07 11:34:08 -0800

    add img to intro

--- src/components/intro/Intro.js
+++ src/components/intro/Intro.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './Intro.css';
+import portrait from '../../assets/portrait.png'
 
 
 function Intro() {
     return (
-        <div>
-            <div className='name'>Hi, I'm Halle King</div>
-            <div id='title'/>
+        <div className='container-div'>
+            <div className='name'>Hi, I'm Halle King<div id='title'></div></div>
+            <img className='portrait' src={portrait}></img>
         </div>
     );
 }
